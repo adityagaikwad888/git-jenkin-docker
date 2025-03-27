76aac7fbd2d8f101cc22f299b7f778564d8a3f38
adityagaikwad888 <adityakg99@gmail.com>
2025-03-27 16:37:09 +0530

    Updated on day 2 run 3

--- test/app.test.js
+++ test/app.test.js
@@ -8,7 +8,7 @@ describe("API Endpoints", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty("message");
     expect(response.body.message).toBe(
-      "Welcome to the APIs on AWS via CI/CD For Cloud Computings"
+      "Welcome to the APIs on AWS via CI/CD For Cloud Computing"
     );
     expect(response.body.status).toBe("success");
   });
